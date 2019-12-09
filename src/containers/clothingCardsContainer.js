154253804b79058e4f78202d55070c3c6a86005f
Marisa Canan-Zucker <mcananzucker@gmail.com>
2019-12-09 14:32:04 -0500

    clean up old comments

--- src/containers/clothingCardsContainer.js
+++ src/containers/clothingCardsContainer.js
@@ -8,47 +8,25 @@ class ClothingCardsContainer extends React.Component {
 
 
   render() {
-    const {items, toggleSelection, selectedItems} = this.props
+    const {items, toggleSelection, selectedItems, closet} = this.props
     return (
 
 
 
       <Card.Group centered stackable>
+
         {items.map(item =>
           <ClothingCard
             key={`closet-item-card-${item.id}`}
             item={item}
             toggleSelection={toggleSelection}
             cardType="main"
+            cardStyle={closet}
           />
         )}
+
       </Card.Group>
   )}
 }
 
 export default ClothingCardsContainer
-
-
-
-
-// {this.state.filteredItems.map(item => <ClothingCard key={item.id} item={item} />)}
-
-
-
-
-
-
-
-
-
-
-// {/* <Card.Group itemsPerRow={4}>
-//   <Card
-//     raised={true}>
-//     <Card.Content>
-//       <Placeholder>
-//         <Placeholder.Image square />
-//       </Placeholder>
-//     </Card.Content>
-//   </Card>
-// </Card.Group> */}
